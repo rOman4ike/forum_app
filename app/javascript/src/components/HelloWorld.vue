54753ab752ed68a5ddafa3ad7b95042ed708df0e
rOman4ike <rOman4ike@yandex.ru>
2023-05-31 23:39:47 +0700

    added Vue

---
+++ app/javascript/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>
+    {{ message }}
+  </p>
+  <h3>asdfasdfasdf</h3>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      message: "hello there"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
