9c88fe7447a4db256eac5ff0f28adc673c59f2df
rOman4ike <rOman4ike@yandex.ru>
2023-06-03 14:26:33 +0700

    added vue2 and typescript

---
+++ app/javascript/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <p>{{ testTS(message) }}</p>
+  </div>
+</template>
+
+<script lang='ts'>
+
+
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!a"
+    }
+  },
+  methods: {
+    testTS(message: string): string {
+      return message + "heheh"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
--- babel.config.js
+++ babel.config.js
@@ -34,7 +34,8 @@ module.exports = function(api) {
           modules: false,
           exclude: ['transform-typeof-symbol']
         }
-      ]
+      ],
+      ["babel-preset-typescript-vue", { "allExtensions": true, "isTSX": true }]
     ].filter(Boolean),
     plugins: [
       'babel-plugin-macros',
