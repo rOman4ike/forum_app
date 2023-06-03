3f36e65ec13e33f5b7c441c4adc68acd8dd74805
rOman4ike <rOman4ike@yandex.ru>
2023-06-03 12:41:57 +0700

    added webpacker && deleted turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,6 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import initVueApp from "@/main.js"
-
-initVueApp()
 
 Rails.start()
 ActiveStorage.start()
